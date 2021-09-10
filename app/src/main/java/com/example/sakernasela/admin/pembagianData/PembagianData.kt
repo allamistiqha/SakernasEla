50866f25ab2b7789cd965ced693ca83e90afb0ac
allamistiqha <alamistqiha@gmail.com>
2021-09-10 11:04:19 -0700

    pembagian data belum fix

--- app/src/main/java/com/example/sakernasela/admin/pembagianData/PembagianData.kt
+++ app/src/main/java/com/example/sakernasela/admin/pembagianData/PembagianData.kt
@@ -3,10 +3,14 @@ package com.example.sakernasela.admin.pembagianData
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.sakernasela.R
+import com.example.sakernasela.databinding.ActivityPembagianDataBinding
 
 class PembagianData : AppCompatActivity() {
+    lateinit var b : ActivityPembagianDataBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pembagian_data)
+        b = ActivityPembagianDataBinding.inflate(layoutInflater)
+        setContentView(b.root)
+
     }
 }
